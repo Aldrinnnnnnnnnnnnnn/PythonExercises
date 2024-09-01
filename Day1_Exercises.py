3490aa4a1f70840a104d04eb3ce9aa5d3476c528
Alju_Tamonte <tamontealdrin28@gmail.com>
2024-09-01 20:19:58 +0800

    Exercise1

--- Day1_Exercises.py
+++ Day1_Exercises.py
@@ -1,1 +1,15 @@
-print("Hello Worl")
+print(type(10)) #int
+print(type(10.99))#float
+print(type(3-9j))#complex
+print(type('I\'m a string'))#string
+print(type(True))#Boolean
+print(type(['Aldrin',10,10.2,3-4j,'I\'m a string',True]))#List
+print(type(('Aldrin',10,10.2,3-4j,'I\'m a string',True)))#Tuple
+print(type({'Aldrin',10,10.2,3-4j,'I\'m a string',True}))#Set
+print(type(my_Dict:= {
+  'FirstName': 'Aldrin',
+  'LastName':'Tamonte',
+  'Number': 4234,
+  'is_Married': False
+}))
+
