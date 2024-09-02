a5db423b0f5f14690ce10e54d6bd4c42a51d4fbf
Alju_Tamonte <tamontealdrin28@gmail.com>
2024-09-02 13:25:27 +0800

    Added Exercise Day2

--- Day1_Exercises.py
+++ Day1_Exercises.py
@@ -1,15 +1,4 @@
-print(type(10)) #int
-print(type(10.99))#float
-print(type(3-9j))#complex
-print(type('I\'m a string'))#string
-print(type(True))#Boolean
-print(type(['Aldrin',10,10.2,3-4j,'I\'m a string',True]))#List
-print(type(('Aldrin',10,10.2,3-4j,'I\'m a string',True)))#Tuple
-print(type({'Aldrin',10,10.2,3-4j,'I\'m a string',True}))#Set
-print(type(my_Dict:= {
-  'FirstName': 'Aldrin',
-  'LastName':'Tamonte',
-  'Number': 4234,
-  'is_Married': False
-}))
+quote3 = 'It\'s a , ',' Ha\'i beautiful day!'
+quote4 = "He said, \"Welcome!\""
 
+print(quote3)
