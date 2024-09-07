05fd04cb3977339855b1868c6f1cbbf9082a8111
Alju_Tamonte <tamontealdrin28@gmail.com>
2024-09-07 19:15:31 +0800

    Added Exercises8

---
+++ Day8Dictionaries_Exercises.py
@@ -0,0 +1,33 @@
+#Exercises: Day 8
+#1.Create an empty dictionary called dog
+#2.Add name, color, breed, legs, age to the dog dictionary
+#3.Create a student dictionary and add first_name, last_name, gender, age, marital status, skills, country, city and address as keys for the dictionary
+#4.Get the length of the student dictionary
+#5.Get the value of skills and check the data type, it should be a list
+#6.Modify the skills values by adding one or two skills
+#7.Get the dictionary keys as a list
+#8.Get the dictionary values as a list
+#9.Change the dictionary to a list of tuples using items() method
+#10.Delete one of the items in the dictionary
+#11.Delete one of the dictionaries
+
+#Set and dictionaries harry problem sets
+#1.Write a program to create a dictionary of Hindi words with value as  their english translation.
+  #Provide user with an option to look it up!
+#2.Write a program to input eight numbers from the user and display all the unique number(once.)
+#3.Can we have a set with 18(int) and "18"(str) as a number as a value in it?
+#4.What will be the length of following set S.
+  #S =Set()
+  #S.add(20)
+  #S.add(20.0)
+  #S.add("20") => length of S after these operations?
+#5. S ={} What is the type of S?
+S = {}
+print(type(S))#<class 'dict'>
+#6.Create an empty dictionary. Allow 4 friends to enter their favourite language as values and use keys as their names.
+  #Assuem that the name are unique.
+#7.If names of 2 friends are same; What will to the program in Problem6?
+#8.If languages  of two friends are same; what will happen to the program in Problem 6?
+#9.Can you change the values inside a list which is contained in set S.
+S = {8,7,12,'Harry',[1,2]}
+S.append(4) = [3]
